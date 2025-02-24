54815c07e865e39cf8f0657d1601bda87e281d31
Ebenezer Kwakye <106906319+EbentheAnalyst@users.noreply.github.com>
2025-02-24 18:40:19 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Open the side navigation
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+}
+
+// Close the side navigation
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+}
